c8cbec49084b1ae7e0a41ecf131a1a44cd3bbc74
Simon Robert <srob@srob.fr>
2024-04-07 12:12:52 +0200

    fix: fizzBuzz() name + more precise types

--- Algo/fizzbuzz.js
+++ Algo/fizzbuzz.js
@@ -5,9 +5,9 @@
  * @see https://github.com/fulll/hiring/blob/master/Algo/fizzbuzz.md
  * @param {Array<{divisor: number, text: string}>} rules
  * @param {number} n The maximum number to process
- * @param {(...data: any) => void} output The output function to use, typically console.log
+ * @param {(text: string|number) => void} output The output function to use, typically console.log
  */
-function fizzbuzz(rules, n, output = console.log) {
+function fizzBuzz(rules, n, output = console.log) {
     for (let i = 1; i <= n; i++) {
         const rulesEvaluationResult = rules
             .map(({ divisor, text }) => (i % divisor === 0 ? text : ""))
@@ -17,11 +17,11 @@ function fizzbuzz(rules, n, output = console.log) {
     }
 }
 
-fizzbuzz(
+fizzBuzz(
     [
         { divisor: 3, text: "Fizz" },
         { divisor: 5, text: "Buzz" },
     ],
-    45
+    15
 );
 
